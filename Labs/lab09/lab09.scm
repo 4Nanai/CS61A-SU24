fbb69326a62d5d92025b7adf9f86e5092156ae63
Chitose Ri <103087652@qq.com>
2024-08-18 22:52:32 -0400

    Update: lab09 scheme

---
+++ Labs/lab09/lab09.scm
@@ -0,0 +1,60 @@
+; (define (over-or-under num1 num2) 
+;   (if (< num1 num2) -1 
+;   (if (= num1 num2) 0 1))
+; )
+
+(define (over-or-under num1 num2) ; Cond
+  (cond ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)
+  )
+)
+
+(define (make-adder num) 
+  (lambda (inc) (+ num inc))
+)
+
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
+
+; (define (repeat f n) 
+;   (cond ((= n 1) (lambda (x) (f x)))
+;     (else (composed f (repeat f (- n 1))))
+;   )
+; )
+
+(define (repeat f n)
+  (if (= n 1) (lambda (x) (f x)) (composed f (repeat f (- n 1)))) ; In one line
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 
+  (if (> a b)
+    (if (not (= 0 (modulo a b)))
+      (gcd b (modulo a b))
+      b)
+    (if (= a b)
+    a
+    (gcd b a)
+    )
+  )
+)
+
+
+(define (duplicate lst) 'YOUR-CODE-HERE)
+
+(expect (duplicate '(1 2 3)) (1 1 2 2 3 3))
+
+(expect (duplicate '(1 1)) (1 1 1 1))
+
+(define (deep-map fn s) 'YOUR-CODE-HERE)
